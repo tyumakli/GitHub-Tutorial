61b5b7e6b2b9a0b9a7bab548ba10c6081ced520d
tugbabozcaga <tugbabozcaga@gmail.com>
2018-11-30 13:28:39 -0500

    Entered my bday

--- Birthdays.R
+++ Birthdays.R
@@ -2,6 +2,6 @@
 ##Demo 3
 
 ##Add your birthday here to the vector below as a new element 
-birthdays <- c("05-02", "05-12", "05-18")
+birthdays <- c("13-07", "05-02")
 birthdays <- format(birthdays, format="%m-%d")
 table(birthdays)
